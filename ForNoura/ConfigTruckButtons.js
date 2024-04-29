e6dc1404b58b21c912833acc9f1ac83b1a6898db
Eng.nouf <124361377+EngllNouf@users.noreply.github.com>
2024-04-29 23:34:19 +0300

    Add by nouf

---
+++ ForNoura/ConfigTruckButtons.js
@@ -0,0 +1,24 @@
+document.getElementById("addTruck-btn").addEventListener("click", createTruckForm);
+document.getElementById("Check").addEventListener("click", enableConfirm);
+
+function createTruckForm() {
+
+    var addFormPlace = document.getElementById('AddForm');
+    var formBody = document.querySelector('.formBody');
+
+    addFormPlace.appendChild(formBody.cloneNode(true));
+}
+
+function enableConfirm(){
+
+    var check = document.getElementById("Check");
+    var confirm = document.getElementById("confirm-btn");
+
+    if(check.checked){
+        confirm.removeAttribute("disabled");
+    }
+    else{
+        confirm.disabled = "true";
+    }
+
+}
