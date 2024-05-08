be117c63530eba720b753371e468b156c65c70a2
P4Zna <132061990+P4Zna@users.noreply.github.com>
2024-05-09 00:55:43 +0300

    Real-time validation update 2

--- website-logistics-system/ConfigureTruck/RealTimeValidation.js
+++ website-logistics-system/ConfigureTruck/RealTimeValidation.js
@@ -9,20 +9,20 @@ function validateInput(input) {
         errorElement.textContent = ""; // مسح رسالة الخطأ
       }
       // تحقق من أن عنصر الإدخال هو OwnerID وأن القيمة ليست من 10 أرقام
-      if (input.id === 'OwnerID' && input.value.trim().length !== 10) {
-        errorElement.textContent = 'Owner ID must be exactly 10 digits';
-        errorElement.style.color = 'red'; // تغيير لون النص إلى أحمر
+      if (input.id === 'OwnerID' && (input.value.trim().length !== 10 || isNaN(input.value))) {
+        errorElement.textContent = 'Owner ID must be exactly 10 digits and contain only numbers';
+        errorElement.style.color = 'red';
       }
 
-      //Validate chassis length
+      //Validate chassis 
       if (input.id === 'VehicleChassis' && input.value.trim().length !== 17) {
         errorElement.textContent = 'Vehicle chassis must be exactly 17 digits';
         errorElement.style.color = 'red'; 
       }
 
-      //Validate serial number length
-      if (input.id === 'Serial_number' && (input.value.trim().length < 8 || input.value.trim().length > 10)) {
-        errorElement.textContent = 'Serial number must be between 8 and 10 digits';
+      //Validate serial number 
+      if (input.id === 'Serial_number' && (input.value.trim().length < 8 || input.value.trim().length > 10 || isNaN(input.value))) {
+        errorElement.textContent = 'Serial number must be between 8 and 10 digits and contain only numbers';
         errorElement.style.color = 'red'; 
       }
 
@@ -31,7 +31,7 @@ function validateInput(input) {
         const currentYear = new Date().getFullYear();
         const yearValue = parseInt(input.value.trim());
       
-        if (input.value.trim().length !== 4 || yearValue > currentYear) {
+        if (input.value.trim().length !== 4 || yearValue > currentYear || isNaN(input.value)) {
           errorElement.textContent = 'Manufacture year must be a valid 4-digit year';
           errorElement.style.color = 'red'; 
         }
@@ -92,22 +92,89 @@ function validateInput(input) {
         }
       }
 
-      //Validate File input
-      if (input.type === 'file') {
-        const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif', 'pdf'];
-        const fileExtension = input.value.split('.').pop().toLowerCase();
-        const isValidFile = allowedExtensions.includes(fileExtension);
+      //Validate Owner name 
+      if (input.id === 'Owner' && (input.value.trim().length < 2 || input.value.trim().length > 20 || !input.value.match(/^[A-Za-z]+$/))) {
+        errorElement.textContent = 'Owner name must be between 2 and 20 digits and contain only letters';
+        errorElement.style.color = 'red'; 
+      }
+
+      // Validate Registration type  
+      if (input.id === 'RegistrationType' && (input.value.trim().length < 2 || input.value.trim().length > 20 || !input.value.match(/^[A-Za-z]+$/))) {
+        errorElement.textContent = 'Registration type must be between 2 and 20 digits and contain only letters';
+        errorElement.style.color = 'red'; 
+      }
+
       
-        if (!isValidFile) {
-          errorElement.textContent = 'File must be either a photo (JPG, JPEG, PNG, GIF) or a PDF';
-          errorElement.style.color = 'red';
-        }
+      //Validate Vehicle Brand  
+      if (input.id === 'VehicleBrand' && (input.value.trim().length < 2 || input.value.trim().length > 20 || !input.value.match(/^[A-Za-z]+$/))) {
+        errorElement.textContent = 'Vehicle Brand must be between 2 and 20 digits and contain only letters';
+        errorElement.style.color = 'red'; 
+      }
+
+      //Validate Vehicle model  
+      if (input.id === 'VehicleModel' && (input.value.trim().length < 2 || input.value.trim().length > 20 || !input.value.match(/^[A-Za-z]+$/))) {
+        errorElement.textContent = 'Vehicle model must be between 2 and 20 digits and contain only letters';
+        errorElement.style.color = 'red'; 
+      }
+
+      //Validate colour 
+      if (input.id === 'Colour' && (input.value.trim().length < 2 || input.value.trim().length > 20 || !input.value.match(/^[A-Za-z]+$/))) {
+        errorElement.textContent = 'Colour must be between 2 and 20 digits and contain only letters';
+        errorElement.style.color = 'red'; 
       }
 
     }
+}
+
+// Validate File Inputs
+const fileInputs = document.querySelectorAll('input[type="file"]');
+const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif', 'pdf'];
+
+fileInputs.forEach(input => {
+  input.addEventListener('change', () => {
+    const errorElement = document.getElementById(input.id + '-error');
+    const fileExtension = input.value.split('.').pop().toLowerCase();
+    const isValidFile = allowedExtensions.includes(fileExtension);
+
+    if (!isValidFile) {
+      errorElement.textContent = 'File must be either a photo (JPG, JPEG, PNG, GIF) or a PDF';
+      errorElement.style.color = 'red';
+    } else {
+      errorElement.textContent = ''; // Clear the error message if the file is valid
+    }
+  });
+});
+  
+  
+// Validate User
+function validateUserInput(input) {
+  const errorElement = document.getElementById(`${input.id}-error`);
+  if (errorElement) {
+    const userValue = input.value.trim();
+
+    if (userValue.length > 0 && (userValue.length < 2 || userValue.length > 20 || !/^[A-Za-z]+$/.test(userValue))) {
+      errorElement.textContent = 'User must be between 2 and 20 characters and contain only letters';
+      errorElement.style.color = 'red';
+    } else {
+      errorElement.textContent = '';
+    }
   }
-  
-  
+}
+
+// Validate User ID
+function validateUserIDInput(input) {
+  const errorElement = document.getElementById(`${input.id}-error`);
+  if (errorElement) {
+    const userIDValue = input.value.trim();
+
+    if (userIDValue.length > 0 && (userIDValue.length !== 10 || isNaN(userIDValue))) {
+      errorElement.textContent = 'User ID must be exactly 10 digits and contain only numbers';
+      errorElement.style.color = 'red';
+    } else {
+      errorElement.textContent = '';
+    }
+  }
+}
   
   
   // Function to validate file input
